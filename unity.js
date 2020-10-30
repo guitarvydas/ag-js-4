701d8883d01339d8dac58ef47042050c554b4303
Paul Tarvydas <guitarvydas@gmail.com>
2020-10-30 13:41:42 -0400

    butchered unity.js for toOrg() function on trace result - edit result in org-mode of emacs

--- unity.js
+++ unity.js
@@ -55,48 +55,123 @@ function toPackedString (a) {
 }
 
 ////////
-// pt trace
-// copy/pasted from ohm-js/src/Trace.js
+// toOrg
+// copy/pasted from ohm-js/src/Trace.js and common.js
 ///////
+repeatFn = function(fn, n) {
+  const arr = [];
+  while (n-- > 0) {
+    arr.push(fn());
+  }
+  return arr;
+};
+
+repeat = function(x, n) {
+  return repeatFn(() => x, n);
+};
+// --------------------------------------------------------------------
+// Private stuff
+// --------------------------------------------------------------------
+// Unicode characters that are used in the `toString` output.
+var BALLOT_X = '\u2717';
+var CHECK_MARK = '\u2713';
+var DOT_OPERATOR = '\u22C5';
+var RIGHTWARDS_DOUBLE_ARROW = '\u21D2';
+var SYMBOL_FOR_HORIZONTAL_TABULATION = '\u2409';
+var SYMBOL_FOR_LINE_FEED = '\u240A';
+var SYMBOL_FOR_CARRIAGE_RETURN = '\u240D';
+var Flags = {
+    succeeded: 1 << 0,
+    isRootNode: 1 << 1,
+    isImplicitSpaces: 1 << 2,
+    isMemoized: 1 << 3,
+    isHeadOfLeftRecursion: 1 << 4,
+    terminatesLR: 1 << 5
+};
+function spaces(n) {
+    return repeat(' ', n).join('');
+}
+function asterisks(n) {
+    return repeat('*', n).join('');
+}
+// Return a string representation of a portion of `input` at offset `pos`.
+// The result will contain exactly `len` characters.
+function getInputExcerpt(input, pos, len) {
+    var excerpt = asEscapedString(input.slice(pos, pos + len));
+    // Pad the output if necessary.
+    if (excerpt.length < len) {
+        return excerpt + repeat(' ', len - excerpt.length).join('');
+    }
+    return excerpt;
+}
+function asEscapedString(obj) {
+    if (typeof obj === 'string') {
+        // Replace non-printable characters with visible symbols.
+        return obj
+            .replace(/ /g, DOT_OPERATOR)
+            .replace(/\t/g, SYMBOL_FOR_HORIZONTAL_TABULATION)
+            .replace(/\n/g, SYMBOL_FOR_LINE_FEED)
+            .replace(/\r/g, SYMBOL_FOR_CARRIAGE_RETURN);
+    }
+    return String(obj);
+}
+
+// StringBuffer - from common.js
+
+StringBuffer = function() {
+  this.strings = [];
+};
+
+StringBuffer.prototype.append = function(str) {
+  this.strings.push(str);
+};
+
+StringBuffer.prototype.contents = function() {
+  return this.strings.join('');
+};
 // Return a string representation of the trace.
 // Sample:
 //     12⋅+⋅2⋅*⋅3 ✓ exp ⇒  "12"
 //     12⋅+⋅2⋅*⋅3   ✓ addExp (LR) ⇒  "12"
 //     12⋅+⋅2⋅*⋅3       ✗ addExp_plus
-// Trace.prototype.toOrg = function () {
-//   const sb = new common.StringBuffer();
-//   this.walk((node, parent, depth) => {
-//     if (!node) {
-//       return this.SKIP;
-//     }
-//     const ctorName = node.expr.constructor.name;
-//     // Don't print anything for Alt nodes.
-//     if (ctorName === 'Alt') {
-//       return; // eslint-disable-line consistent-return
-//     }
-//     sb.append(getInputExcerpt(node.input, node.pos, 10) + spaces(depth * 2 + 1));
-//     sb.append((node.succeeded ? CHECK_MARK : BALLOT_X) + ' ' + node.displayString);
-//     if (node.isHeadOfLeftRecursion) {
-//       sb.append(' (LR)');
-//     }
-//     if (node.succeeded) {
-//       const contents = asEscapedString(node.source.contents);
-//       sb.append(' ' + RIGHTWARDS_DOUBLE_ARROW + '  ');
-//       sb.append(typeof contents === 'string' ? '"' + contents + '"' : contents);
-//     }
-//     sb.append('\n');
-//   });
-//   return sb.contents();
-// };
+toOrgFunction = function () {
+    // same as toString, except we prepend an appropriate number of "*"s to the front of each line
+    // save the result in a file with .org extension, then edit it with emacs (it should enter org-mode)
+    // org-mode - used TAB key on line with *'s, to expand/contract the line
+  const sb = new StringBuffer();
+  this.walk((node, parent, depth) => {
+    if (!node) {
+      return this.SKIP;
+    }
+    const ctorName = node.expr.constructor.name;
+    // Don't print anything for Alt nodes.
+    if (ctorName === 'Alt') {
+      return; // eslint-disable-line consistent-return
+    }
+      sb.append(asterisks(depth * 2 + 1) + " " + getInputExcerpt(node.input, node.pos, 10) + spaces(depth * 2 + 1));
+    sb.append((node.succeeded ? CHECK_MARK : BALLOT_X) + ' ' + node.displayString);
+    if (node.isHeadOfLeftRecursion) {
+      sb.append(' (LR)');
+    }
+    if (node.succeeded) {
+      const contents = asEscapedString(node.source.contents);
+      sb.append(' ' + RIGHTWARDS_DOUBLE_ARROW + '  ');
+      sb.append(typeof contents === 'string' ? '"' + contents + '"' : contents);
+    }
+    sb.append('\n');
+  });
+  return sb.contents();
+};
 ////////
-// end pt trace
+// end toOrg
 ///////
 
 if (unityParseTree.failed ()) {
     
     console.log ("Matching Failed")
     var tr = grammar.trace (input);
-    console.log (tr.toString ());
+    tr.toOrg = toOrgFunction;
+    console.log (tr.toOrg ());
 
 } else {
     console.log ("Matching Succeeded")
