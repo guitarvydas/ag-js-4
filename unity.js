df7455b7230cc18d7e6c1ad0b8b782d2aa9ea617
Paul Tarvydas <guitarvydas@gmail.com>
2020-10-29 22:26:45 -0400

    HTML fule and BodyStuff rule

--- unity.js
+++ unity.js
@@ -11,10 +11,11 @@ const ohm = require ('ohm-js')
 
 const unityGrammar = `
 HTMLUnity {
-    html = htmlElement headerStuff bodyElement bodyStuff bodyElementEnd htmlEnd
+    HTML = htmlElement headerStuff bodyElement BodyStuff bodyElementEnd htmlEnd
     htmlElement = "<html>" spaces
     headerStuff = notBody*
     bodyElement = "<body>" spaces
+    BodyStuff = bodyStuff
     bodyStuff = notBodyEnd*	
     notBody = ~"<body>" any
     notBodyEnd = ~"</body>" any
@@ -23,43 +24,44 @@ HTMLUnity {
 }
 `;
 
-const uGrammar = ohm.grammar(unityGrammar);
-var args = require('minimist')(process.argv.slice(2));
+const uGrammar = ohm.grammar (unityGrammar);
+var args = require('minimist') (process.argv.slice (2));
 var inputFilename = args['input'];
 const input = fs.readFileSync("./" + inputFilename);
-const unityParseTree = uGrammar.match(input);
+const unityParseTree = uGrammar.match (input);
 
-function toPackedString(a) {
-    return a.join('');
+function toPackedString (a) {
+    return a.join ('');
 }
 
-if (unityParseTree.failed()) {
+if (unityParseTree.failed ()) {
 
-    console.log("Matching Failed")
-    console.log(uGrammar.trace(input).toString());
+    console.log ("Matching Failed")
+    console.log (uGrammar.trace (input).toString ());
 
 } else {
-    console.log("Matching Succeeded")
-    const semantics = uGrammar.createSemantics()
+    console.log ("Matching Succeeded")
+    const semantics = uGrammar.createSemantics ()
     semantics.addOperation (
 	'unity',
 	{
-	    html: function (htmlElement, headerStuff, bodyElement, bodyStuff, bodyElementEnd, htmlEnd) {
-		return htmlElement.unity() + headerStuff.unity() + bodyElement.unity() + bodyStuff.unity() + bodyElementEnd.unity() + htmlEnd.unity(); 
+	    HTML: function (htmlElement, headerStuff, bodyElement, bodyStuff, bodyElementEnd, htmlEnd) {
+		return htmlElement.unity () + headerStuff.unity () + bodyElement.unity () + bodyStuff.unity () + bodyElementEnd.unity () + htmlEnd.unity (); 
 	    },
-	    htmlElement: function (html, spaces) { return "<html>" + spaces.unity().join(''); },
-	    headerStuff: function (stuff) { return stuff.unity().join(''); },
-	    bodyElement: function (body, spaces) { return "<body>" + spaces.unity().join('')},
-	    bodyStuff: function (stuff) { return stuff.unity().join(''); },
-	    notBody:function (c) { return c.unity(); },
-	    notBodyEnd: function (c) { return c.unity(); },
-	    bodyElementEnd: function (body, spaces) { return "</body>" + spaces.unity().join('');},
-	    htmlEnd: function (html, spaces) { return "</html>" + spaces.unity().join('');},
+	    htmlElement: function (html, spaces) { return "<html>" + spaces.unity ().join (''); },
+	    headerStuff: function (stuff) { return stuff.unity ().join (''); },
+	    bodyElement: function (body, spaces) { return "<body>" + spaces.unity ().join ('')},
+	    BodyStuff: function (stuff) { return stuff.unity (); },
+	    bodyStuff: function (stuff) { return stuff.unity ().join (''); },
+	    notBody:function (c) { return c.unity (); },
+	    notBodyEnd: function (c) { return c.unity (); },
+	    bodyElementEnd: function (body, spaces) { return "</body>" + spaces.unity ().join ('');},
+	    htmlEnd: function (html, spaces) { return "</html>" + spaces.unity ().join ('');},
 
 	    _terminal: function () { return this.primitiveValue; }
 	});
     
-    console.log(semantics(unityParseTree).unity());
+    console.log (semantics (unityParseTree).unity ());
 }
 
 	
